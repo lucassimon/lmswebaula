99c4bb750525019eb442c3f71015cb45a31add6d
Lucas Simon <lucassrod@gmail.com>
2017-04-19 14:08:20 -0300

    Cria container para Error Response

---
+++ lms/core/containers/error.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import six
+
+from lmswebaula.lms.core.containers.response import (
+    ContainerResponse, ErrorListResponse
+)
+
+
+class ErrorRS(ContainerResponse):
+
+    _exception = None
+
+    @property
+    def exception(self):
+        return self._exception
+
+    @exception.setter
+    def exception(self, value):
+
+        self._exception = value
