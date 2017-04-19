1fe1857f241e0d03e686bd2bd980d8e304883544
Lucas Simon <lucassrod@gmail.com>
2017-04-19 14:07:23 -0300

    Remove a classe EnrollmentParse

--- lms/enrollment/parse.py
+++ lms/enrollment/parse.py
@@ -1,15 +1,3 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import pytest
-
-from lmswebaula.lms.student.containers.response import (
-    StudentDTO
-)
-
-
-class EnrollmentParse(object):
-
-    @staticmethod
-    def get_all(response):
-
-        pass
