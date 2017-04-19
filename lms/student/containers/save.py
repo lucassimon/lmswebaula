a0ddac921d6ff5b942d7cc3b70eeab8af4b0b965
Lucas Simon <lucassrod@gmail.com>
2017-04-19 11:19:23 -0300

    Cria os containers de SaveRQ e SaveRS para o estudante

--- lms/student/containers/save.py
+++ lms/student/containers/save.py
@@ -7,7 +7,7 @@ from lmswebaula.lms.core.containers.response import (
 )
 
 
-class Student(object):
+class SaveRQ(object):
 
     _lms_student_id = ''
     _student_id = ''
@@ -18,7 +18,7 @@ class Student(object):
     _sex = ''
     _admission_date = ''
     _cpf = ''
-    _status = ''
+    _status = True
     _password = ''
     _date_import = ''
     _address = ''
@@ -34,5 +34,105 @@ class Student(object):
     _nickname = ''
     _company = ''
 
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        name,
+        email,
+        cpf,
+        password,
+        registration=None
+    ):
+        self._name = name
+        self._email = email
+        self._cpf = cpf
+        self._password = password
+
+        if registration is None:
+            self._registration = cpf
+        else:
+            self._registration = registration
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+
+        self._name = value
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, value):
+
+        self._email = value
+
+    @property
+    def cpf(self):
+        return self._cpf
+
+    @cpf.setter
+    def cpf(self, value):
+
+        self._cpf = value
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, value):
+
+        self._password = value
+
+    @property
+    def registration(self):
+        return self._registration
+
+    @registration.setter
+    def registration(self, value):
+
+        self._registration = value
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, value):
+
+        self._status = value
+
+
+class SaveRS(ContainerResponse):
+    """
+    Resposta do metodo save
+    """
+
+    _data_list = []
+
+    def __init__(self, error=True, guid='', msg='', data=[]):
+
+        if not isinstance(data, list):
+            raise ValueError(
+                'Os estudantes precisam estar em uma lista'
+            )
+
+        self._data_list = data
+
+    @property
+    def data_list(self):
+        return self._data_list
+
+    @data_list.setter
+    def data_list(self, value):
+
+        if not isinstance(value, list):
+            raise ValueError(
+                'Os estudantes precisam estar em uma lista'
+            )
+
+        self._data_list = value
