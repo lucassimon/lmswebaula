1f460afd67aeff735a37de90f0305ec9e7cb5716
Lucas Simon <lucassrod@gmail.com>
2017-04-19 09:38:45 -0300

    Padroniza o nome das variaveis

--- lms/course/parse.py
+++ lms/course/parse.py
@@ -2,14 +2,29 @@
 from __future__ import unicode_literals
 import pytest
 
+from lmswebaula.lms.course.containers import *
+
 
 class CourseParse(object):
 
     @staticmethod
     def get_all(response):
 
-        courses = []
+        data = []
 
-        pytest.set_trace()
+        ws_students = response['CourseListDTO']['CourseDTO']
 
-        return courses
+        for std in ws_students:
+
+            data.append(
+                CourseDTO(
+                    folder_name=std['FolderName'],
+                    frequency=float(std['Frequency']),
+                    hours=float(std['Hours']),
+                    media=float(std['Media']),
+                    name=std['Name'],
+                    name_course_menu=std['NameCourseMenu']
+                )
+            )
+
+        return data
