569d8421bac16cf556b292ed86880695cf3fbdc7
Lucas Simon <lucassrod@gmail.com>
2017-04-19 09:36:01 -0300

    Seta os getters e setter da Classe ContainerResponse

--- lms/core/containers/response.py
+++ lms/core/containers/response.py
@@ -13,6 +13,33 @@ class ContainerResponse(object):
         self._guid = guid
         self._msg = msg
 
+    @property
+    def has_error(self):
+        return self._has_error
+
+    @has_error.setter
+    def has_error(self, value):
+
+        self._has_error = value
+
+    @property
+    def guid(self):
+        return self._guid
+
+    @guid.setter
+    def guid(self, value):
+
+        self._guid = value
+
+    @property
+    def msg(self):
+        return self._msg
+
+    @msg.setter
+    def msg(self, value):
+
+        self._msg = value
+
 
 class ErrorListResponse(object):
 
