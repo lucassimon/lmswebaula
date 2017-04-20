6d063b2ba9c8751ec8b07a7a338bd2b21317e9b5
Lucas Simon <lucassrod@gmail.com>
2017-04-20 17:26:30 -0300

    Cria o container get_by_course_id

---
+++ lms/klass/containers/get_by_course_id.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import six
+
+
+class GetByCourseIdRQ(object):
+
+    _course_id = None
+    _lms_course_id = None
+
+    def __init__(self, lms_course_id, course_id=None):
+
+        self._lms_course_id = lms_course_id
+        self._course_id = course_id
+
+    @property
+    def lms_course_id(self):
+        return self._lms_course_id
+
+    @lms_course_id.setter
+    def lms_course_id(self, value):
+
+        self._lms_course_id = value
+
+    @property
+    def course_id(self):
+        return self._course_id
+
+    @course_id.setter
+    def course_id(self, value):
+
+        self._course_id = value
