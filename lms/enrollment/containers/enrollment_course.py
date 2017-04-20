66116c9a0100e6f1a0aadfb2ef8b5d31770e0ab8
Lucas Simon <lucassrod@gmail.com>
2017-04-20 09:14:17 -0300

    Cria as variaveis student_id para matricular aluno

--- lms/enrollment/containers/enrollment_course.py
+++ lms/enrollment/containers/enrollment_course.py
@@ -13,10 +13,23 @@ class EnrollmentCourseRQ(object):
 
     _lms_class_id = None
 
-    def __init__(self, lms_student_id, lms_class_id):
+    _class_id = None
+
+    _student_id = None
+
+    def __init__(
+        self,
+        lms_student_id=None,
+        lms_class_id=None,
+        class_id=None,
+        student_id=None
+    ):
 
         self._lms_student_id = lms_student_id
         self._lms_class_id = lms_class_id
+
+        self._class_id = class_id
+        self._student_id = student_id
 
     @property
     def lms_student_id(self):
@@ -35,6 +48,24 @@ class EnrollmentCourseRQ(object):
     def lms_class_id(self, value):
 
         self._lms_class_id = value
+
+    @property
+    def student_id(self):
+        return self._student_id
+
+    @student_id.setter
+    def student_id(self, value):
+
+        self._student_id = value
+
+    @property
+    def class_id(self):
+        return self._class_id
+
+    @class_id.setter
+    def class_id(self, value):
+
+        self._class_id = value
 
 
 class EnrollmentCourseRS(ContainerResponse):
