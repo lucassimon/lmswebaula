b59ce93d3b241be7ee7d719ac475d9be2bbb70d1
Lucas Simon <lucassrod@gmail.com>
2017-05-25 12:19:36 -0300

    Insere container SuccessContainerResponse

--- lms/trail/containers/get_trail.py
+++ lms/trail/containers/get_trail.py
@@ -5,7 +5,7 @@ import six
 
 from lms.core.containers.pagination import PaginationMixinRQ
 from lms.core.containers.response import (
-    ContainerResponse
+    SuccessContainerResponse
 )
 
 
@@ -38,37 +38,11 @@ class GetTrailRQ(object):
         self._trail_id = value
 
 
-class GetTrailRS(ContainerResponse):
+class GetTrailRS(SuccessContainerResponse):
     """
 
     Url: http://lmsapi.webaula.com.br/v3/DOC/API.aspx?s=Trail&m=GetTrail
 
     """
 
-    _data_list = []
-
-    def __init__(self, error=True, guid='', msg='', data=[]):
-
-        if not isinstance(data, list):
-            raise ValueError(
-                'As trilhas precisam estar em uma lista'
-            )
-
-        self._data_list = data
-        self._has_error = error
-        self._guid = guid
-        self._msg = msg
-
-    @property
-    def data_list(self):
-        return self._data_list
-
-    @data_list.setter
-    def data_list(self, value):
-
-        if not isinstance(value, list):
-            raise ValueError(
-                'As trilhas precisam estar em uma lista'
-            )
-
-        self._data_list = value
+    pass
