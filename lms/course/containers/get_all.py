e29b4b33cc1c3783f87f634a8e0c4028f49e1807
Lucas Simon <lucassrod@gmail.com>
2017-05-30 15:13:18 -0300

    Cria getter e setter do atricuto pagination

--- lms/course/containers/get_all.py
+++ lms/course/containers/get_all.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 import pytest
 import six
 
-from lms.core.containers.pagination import PaginationMixinRQ
+from lms.core.containers.pagination import PaginationMixinRQ, PaginationMixinRS
 from lms.core.containers.response import (
     SuccessContainerResponse
 )
@@ -20,4 +20,13 @@ class GetAllCourseRS(SuccessContainerResponse):
     Resposta da requisição do get_all
     """
 
-    pass
+    _pagination = None
+
+    @property
+    def pagination(self):
+        return self._pagination
+
+    @pagination.setter
+    def pagination(self, pagination_rs):
+
+        self._pagination = pagination_rs
