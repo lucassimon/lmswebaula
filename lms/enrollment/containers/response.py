6952570289247ce465a3f9bf6854953e895c9bed
Lucas Simon <lucassrod@gmail.com>
2017-07-17 13:10:13 -0300

    Cria objeto EnrollmentStudent

--- lms/enrollment/containers/response.py
+++ lms/enrollment/containers/response.py
@@ -6,3 +6,47 @@ import datetime
 from lms.core.containers.response import (
     ContainerResponse, ErrorListResponse
 )
+
+
+class StudentEnrolledInTrailDTO(object):
+
+    _state = None
+
+    _ffrom = None
+
+    _to = None
+
+    def __init__(self, state, ffrom, to):
+
+        self._state = state
+
+        self._ffrom = ffrom
+
+        self._to = to
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, value):
+
+        self._state = value
+
+    @property
+    def ffrom(self):
+        return self._ffrom
+
+    @ffrom.setter
+    def ffrom(self, value):
+
+        self._ffrom = value
+
+    @property
+    def to(self):
+        return self._to
+
+    @to.setter
+    def to(self, value):
+
+        self._to = value
