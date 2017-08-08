7cd9aff428dd285f7bc6c5eb058154beac5642ea
Lucas Simon <lucassrod@gmail.com>
2017-08-07 22:09:12 -0300

    Importa container

--- lms/enrollment/containers/__init__.py
+++ lms/enrollment/containers/__init__.py
@@ -3,3 +3,4 @@ from .response import *
 from .set_status import *
 from .checks_student_enrolled_in_trail_class import *
 from .checks_student_enrolled_in_trail_default_class import *
+from .get_trails_history_by_class_period import *
